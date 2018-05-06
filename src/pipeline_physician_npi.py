f8a574a994739678c1cc49230b9d35d2bb11c410
Guilherme Pinho <gpinho@gmail.com>
2018-05-06 12:42:51 -0700

    coordinates

--- src/pipeline_physician_npi.py
+++ src/pipeline_physician_npi.py
@@ -1,6 +1,42 @@
+from geopy.geocoders import Nominatim
+import pandas as pd
 
+def get_coordinates(npi_file, usecols):
+
+    df = pd.read_csv(npi_file, usecols=usecols, index_col=['NPI'])
+
+    d = pd.to_dict()
+
+    geolocator = Nominatim()
+
+    for index, row in df.iterrows():
+        first_line = row['Provider First Line Business Mailing Address']
+        second_line = row['Provider Second Line Business Mailing Address']
+        city = row['Provider Business Mailing Address City Name']
+        state = row['Provider Business Mailing Address State Name']
+        postal_code = row['Provider Business Mailing Address Postal Code']
+        country = row['Provider Business Mailing Address Country Code (If outside US)']
+        try:
+            address = f"{first_line} {city} {state} {country}"
+            location = geolocator.geocode(address)
+            latitude, longitude = location.latitude, location.longitude
+            d['Latitude'][index] = latitude
+            d['Longitude'][index] = longitude
+        except:
+            address = f"{city} {state} {country}"
+            location = geolocator.geocode(address)
+            d['Latitude'][index] = latitude
+            d['Longitude'][index] = longitude
+
+    df = pd.DataFrame.from_dict(d)
+    df.to_csv('npidata_pfile_20050523-20180408_practiceLocation.csv')
 
 if __name__ == '__main__':
+
+    npi_file = 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/npidata_pfile_20050523-20180408_withHeader.csv'
+    usecols = ['NPI', 'Provider First Line Business Mailing Address',
+    'Provider Business Mailing Address City Name', 'Provider Business Mailing Address State Name', 'Provider Business Mailing Address Postal Code', 'Provider Business Mailing Address Country Code (If outside US)']
+    get_coordinates(npi_file, usecols)
     # feature engineer npi dataset
     # npi_usecols = ['NPI', 'Provider Gender Code','Healthcare Provider Taxonomy Code_1']
     # npi_df = pd.read_csv("https://s3-us-west-1.amazonaws.com/physician-referral-graph/npidata_pfile_20050523-20180408_withHeader.csv", index_col=['NPI'], usecols=npi_usecols)
