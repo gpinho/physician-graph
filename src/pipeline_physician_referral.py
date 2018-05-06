06d64c2a12238af5009771992178df2b2ad5a8b3
Ubuntu <ubuntu@ip-172-31-30-73.us-west-1.compute.internal>
2018-05-06 00:51:48 +0000

    test referral pipe locally

--- src/pipeline_physician_referral.py
+++ src/pipeline_physician_referral.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def combine_datasets(left_filepath, left_year, list_right_filepath_year, index_col=['Initial Physician NPI', 'Secondary Physician NPI']):
+def combine_referral_datasets(left_filepath, left_year, list_right_filepath_year, index_col=['Initial Physician NPI', 'Secondary Physician NPI']):
     left_df = pd.read_csv(left_filepath, index_col=index_col)
     left_df.columns += '_' + left_year
     for right_file in list_right_filepath_year:
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     # feature engineer referral dataset
     initial_referral_dataset = 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2009-days180_withHeader.csv'
     initial_referral_year = '2009'
-    other_referral_datasets_years = [('2010','https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2010-days180_withHeader.csv'), ('2011', 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2011-days180_withHeader.csv'), ('2012', 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2012-days180_withHeader.csv'), ('2013', 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2013-days180_withHeader.csv'), ('2014', 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2014-days180_withHeader.csv'), ('2015: ', 'https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2015-days180_withHeader.csv')]
-    referral_df = combine_referral_datasets(initial_referral_year, initial_referral_dataset, other_referral_datasets_years)
+    other_referral_datasets_years = [('https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2010-days180_withHeader.csv', '2010'), ('https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2011-days180_withHeader.csv', '2011'), ('https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2012-days180_withHeader.csv', '2012'), ('https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2013-days180_withHeader.csv', '2013'), ('https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2014-days180_withHeader.csv', '2014'), ('https://s3-us-west-1.amazonaws.com/physician-referral-graph/physician-shared-patient-patterns-2015-days180_withHeader.csv', '2015')]
+    referral_df = combine_referral_datasets(initial_referral_dataset, initial_referral_year, other_referral_datasets_years)
     referral_df.to_csv('physician-shared-patient-patterns-2009-2015-days180_withHeader.csv')
     save_files_to_s3(['physician-shared-patient-patterns-2009-2015-days180_withHeader.csv'], 'physician-referral-graph')
